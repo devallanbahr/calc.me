fd400778c48dd345ed119dac3817e13c95c1bcc6
Allan <allanbahrdev@gmail.com>
2021-11-04 02:56:12 -0300

    default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,40 @@ import React, { Component } from "react";
 import "../stylesheets/App.css";
 
 class App extends Component {
-  state = {
-    counter: 0,
-  };
+  constructor(props) {
+    super(props);
+
+    this.sectionAppContent = React.createRef();
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
   render() {
-    return <section className="app"></section>;
+    const setOrNotVisible = () => {
+      if (this.sectionAppContent.current.style.display === "inline-block")
+        this.sectionAppContent.current.style.display = "none";
+      else {
+        this.sectionAppContent.current.style.display = "inline-block";
+      }
+    };
+    return (
+      <section className="app">
+        <section className="app-input">
+          <input
+            className="app-show"
+            onClick={setOrNotVisible}
+            type="button"
+            value="Show/Hide"
+          />
+        </section>
+        <section ref={this.sectionAppContent} className="app-content">
+          <h2>{this.state.counter}</h2>
+          <input type="text" ref={this.inputText} />
+        </section>
+      </section>
+    );
   }
 }
 
