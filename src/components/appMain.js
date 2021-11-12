b8ca23f3e592bfd7b373689ad45db488367ad036
Allan <allanbahrdev@gmail.com>
2021-11-12 10:55:23 -0300

    default

--- src/components/appMain.js
+++ src/components/appMain.js
@@ -1,52 +1,47 @@
 import React, { Component } from "react";
-import { Container, Main, AppMain, Buttons } from "../stylesheets/appStyle";
+import {
+  Container,
+  Main,
+  AppMain,
+  Buttons,
+  Button,
+  Input,
+} from "../stylesheets/appStyle";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.sectionAppContent = React.createRef();
     this.inputText = React.createRef();
-
+    this.Button = React.createRef();
     this.state = {
-      input: 0,
+      inputText: 0,
     };
   }
 
   render() {
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      this.setState({ input: this.inputText.current.value });
-      console.log(this.state.input);
-    };
     return (
       <AppMain>
         <Main />
-        <Container ref={this.sectionAppContent} className="app-content">
-          <input
-            onChange={handleSubmit}
-            type="number"
-            readOnly={true}
-            ref={this.inputText}
-            value={0}
-          />
+        <Container>
+          <Input type="text" readOnly ref={this.inputText} placeholder={0} />
           <Buttons>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
-            <input type="button"></input>
+            <Button ref={this.Button} type="button" value="1" />
+            <Button ref={this.Button} type="button" value="2" />
+            <Button ref={this.Button} type="button" value="3" />
+            <Button ref={this.Button} type="button" value="+" />
+            <Button ref={this.Button} type="button" value="4" />
+            <Button ref={this.Button} type="button" value="5" />
+            <Button ref={this.Button} type="button" value="6" />
+            <Button ref={this.Button} type="button" value="-" />
+            <Button ref={this.Button} type="button" value="7" />
+            <Button ref={this.Button} type="button" value="8" />
+            <Button ref={this.Button} type="button" value="9" />
+            <Button ref={this.Button} type="button" value="x" />
+            <Button ref={this.Button} type="button" value="CE" />
+            <Button ref={this.Button} type="button" value="0" />
+            <Button ref={this.Button} type="button" value="." />
+            <Button ref={this.Button} type="button" value="=" />
           </Buttons>
         </Container>
       </AppMain>
--- src/stylesheets/appStyle.js
+++ src/stylesheets/appStyle.js
@@ -17,10 +17,26 @@ export const AppMain = styled.div``;
 export const Buttons = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  margin: 1rem;
 `;
-export const Button = styled.a`
-  text-decoration: none;
-  padding: 1.2rem;
+export const Button = styled.input`
+  padding: 1rem;
+  font-size: 14pt;
+  margin: 0.25rem;
+
+  :hover {
+  }
+`;
+
+export const Input = styled.input`
+  width: 265px;
+  padding-left: 0.5rem;
+  height: 8.5vh;
+  font-size: 18pt;
+
+  ::placeholder {
+    color: #000;
+  }
 `;
 
 export const Container = styled.div`
