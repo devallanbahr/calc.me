11c0229f616ee26967c377cf49cf8023976b93ef
Allan <allanbahrdev@gmail.com>
2021-11-14 01:09:54 -0300

    master

--- src/stylesheets/appStyle.js
+++ src/stylesheets/appStyle.js
@@ -1,15 +1,18 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 export const Main = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300;400;500;700&family=Roboto:wght@300;400;700&display=swap');
   *{
     margin: 0 auto;
     padding: 0;
     box-sizing: border-box;
     font-family: "Roboto Mono";
+    outline: none;
+    user-select: none;
   }
 
   body{
-    background-color: #121212;
+    background-color: #121212;  
   }
 
 `;
@@ -22,15 +25,17 @@ export const Buttons = styled.div`
 `;
 export const Button = styled.input`
   padding: 1rem;
+  cursor: pointer;
   font-size: 14pt;
   margin: 0.25rem;
-
+  font-weight: 400;
   :hover {
   }
 `;
 
 export const Input = styled.input`
   width: 265px;
+  font-weight: 300;
   padding-left: 0.5rem;
   height: 8.5vh;
   font-size: 18pt;
